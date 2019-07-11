800a68f4519a18f2be8ded25da130a1813f9e22e
unknown <amandalragone@gmail.com>
2019-07-10 22:12:39 -0500

    Added React components.

---
+++ client/src/components/ArticleResults/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ArticleResults extends React.Component {
+
+    
+    render() {
+
+        return (
+
+            <div>This will be results!</div>
+
+        )
+    
+    }
+
+}
+
+export default ArticleResults;
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+
+    render() {
+
+        return (
+
+            <footer>Copyright © 2019 All rights reserved.</footer>
+        )
+    }
+}
+
+export default Footer;
